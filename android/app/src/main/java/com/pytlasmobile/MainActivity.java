dcbfbbc2817686507b62c006109536ece45a785d
Julien LEICHER <jleicher@gmail.com>
2019-01-30 10:02:50 +0100

    Added icon and splash screen on android

--- android/app/src/main/java/com/pytlasmobile/MainActivity.java
+++ android/app/src/main/java/com/pytlasmobile/MainActivity.java
@@ -1,7 +1,22 @@
 package com.pytlasmobile;
 
 import com.reactnativenavigation.NavigationActivity;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
 
 public class MainActivity extends NavigationActivity {
+
+  @Override
+  protected void addDefaultSplashLayout() {
+    // LinearLayout splash = new LinearLayout(this);
+    // Drawable launch_screen_bitmap = ContextCompat.getDrawable(getApplicationContext(),R.drawable.launch_screen_bitmap);
+    // setContentView(splash);
+    View view = new View(this);
+    view.setBackgroundResource(R.drawable.launch_screen_bitmap);
+
+    setContentView(view);
+  }
 
 }
